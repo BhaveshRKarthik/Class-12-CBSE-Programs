356d240ab839fd625d8c4df16a5adc1425c43e3a
BhaveshRKarthik <109363389+BhaveshRKarthik@users.noreply.github.com>
2022-07-15 22:16:33 +0530

    Finished editing Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,27 @@
 import os
+
+while True:
+    op = input("Insert operator: (one of +, -, *, /, //, **, %):\t")
+    a = input("Insert first number:\t")
+    b = input("Insert second number:\t")
+    if op == '+':
+        print(f"{a + b = }")
+    elif op == '-':
+        print(f"{a - b = }")
+    elif op == '*':
+        print(f"{a * b = }")
+    elif op == '/':
+        print(f"{a / b = }")
+    elif op == '//':
+        print(f"{a // b = }")
+    elif op == '**':
+        print(f"{a ** b = }")
+    elif op == '%':
+        print(f"{a % b = }")
+    else:
+        print("Invalid operator.")
+    resp = input("Do you wish to continue? y/n:\t").lower()
+    if resp != 'y' and resp != 'yes':
+        break
+    else:
+        print('-' * (os.get_terminal_size().columns - 1))
